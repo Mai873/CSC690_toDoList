88555ca9986fc1ce3ecb60f7ddf7dd79a2176747
Mai Ra <mra1@mail.sfsu.edu>
2021-12-09 19:53:35 -0800

    fist commit

---
+++ CSC690_toDoList/CSC690_toDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CSC690_toDoList
+//
+//  Created by Mai Ra on 12/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CSC690_toDoList/CSC690_toDoListTests/CSC690_toDoListTests.swift
@@ -0,0 +1,33 @@
+//
+//  CSC690_toDoListTests.swift
+//  CSC690_toDoListTests
+//
+//  Created by Mai Ra on 12/9/21.
+//
+
+import XCTest
+@testable import CSC690_toDoList
+
+class CSC690_toDoListTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
